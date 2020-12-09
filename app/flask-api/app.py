a9657af98a5588cc6bc6de733ea4ec1d64ec9030
Thomas Quiviger <thomas.quiviger@saagie.com>
2020-12-09 11:11:01 +0100

    Adding a sdk route

--- app/flask-api/app.py
+++ app/flask-api/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def welcome():
     return 'Flask API for Saagie'
 
+@app.route('/sdk')
+def sdk():
+    return 'Saagie Training on SDK'
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=5000)
